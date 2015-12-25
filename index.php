146a96f72ba34a71835cf3cd81547fa764e60312
Snake Man <snakeman@phnet.fi>
2015-12-25 16:35:50 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Command Modern Air/Naval Operations (CMANO) - PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Command Modern Air/Naval Operations (CMANO) - PMC Tactical">
+<META name="keywords" content="Command, Modern, Air, Naval, Operations, CMANO, PMC, Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--
+<?php include("include/google-analytics.php"); ?>
+-->
+
+</head>
+<body>
+
+<!--
+<?php include("include/google-ads.php"); ?>
+-->
+
+<header>
+<h1>PMC Tactical - Command Modern Air/Naval Operations (CMANO)</h1>
+</header>
+
+<section>
+<h2>Command Modern Air/Naval Operations (CMANO)</h2>
+<p>It all started to game called Harpoon back in the 1989.
+</p>
+
+<p>CMANO is developed by Warfare Sims, published by Matrix Games and released on September 24, 2013.
+</p>
+
+<p>Info on this page is based on v1.09 Final - Build 757.2
+</p>
+
+</section>
+
+<footer>
+<p><i>2015 - <?print(date("Y"));?>.</i></p>
+</footer>
+
+</body>
+</html>

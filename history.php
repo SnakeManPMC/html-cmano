f40fc89d7675563f62de07527f7a198254f69ef2
Snake Man <snakeman@phnet.fi>
2017-11-07 21:52:05 +0200

    Full release history added.

--- history.php
+++ history.php
@@ -22,11 +22,87 @@
 	<h2>History</h2>
 
 <p>
-Northern Inferno
+<b>09-24-13</b> Command: Modern Air/Naval Operations v1.0 Release!
 </p>
 
 <p>
-<b>05-18-17</b> CMANO: Chains of War was released. v1.12 Build 936.11
+<b>11-10-13</b> Command Build 460
+</p>
+
+<p>
+<b>11-29-13</b> Command Build 469
+</p>
+
+<p>
+<b>12-19-13</b> Command v1.02
+</p>
+
+<p>
+<b>08-28-14</b> Command Build 562
+</p>
+
+<p>
+<b>12-17-15</b> Command v1.06
+</p>
+
+<p>
+<b>03-09-15</b> Command v1.07
+</p>
+
+<p>
+<b>07-14-15</b> Command v1.08
+</p>
+
+<p>
+<b>10-22-15</b> Northern Inferno, standalone expansion
+</p>
+
+<p>
+<b>02-26-16</b> Command v1.10
+</p>
+
+<p>
+<b>05-09-16</b> Command v1.11
+</p>
+
+<p>
+<b>07-19-16</b> Command Live #1 Old Grudges Never Die!
+</p>
+
+<p>
+<b>08-23-16</b> Command Live #2 You Brexit It, You Fix It!
+</p>
+
+<p>
+<b>09-12-16</b> Command Live #3 Spratly Spat
+</p>
+
+<p>
+<b>09-20-16</b> v1.11 Service Release 5 update
+</p>
+
+<p>
+<b>11-02-16</b> Command Live #4 Don of a New Era
+</p>
+
+<p>
+<b>02-28-17</b> Command Live #5 Korean Missile Crisis
+</p>
+
+<p>
+<b>05-18-17</b> Chains of War was released. v1.12 Build 936.11
+</p>
+
+<p>
+<b>07-17-17</b> Command Live #6 Pole Positions
+</p>
+
+<p>
+<b>10-24-17</b> Shifting Sands, standalone DLC v1.13
+</p>
+
+<p>
+<b>11-07-17</b> Command Live #7 Black Gold Blitz expansion (scenario or few?).
 </p>
 
 </section>

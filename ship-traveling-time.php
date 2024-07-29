916ba2244c624fd58bf145ca447f1d9dca7f0e16
Snake Man <snakeman@phnet.fi>
2024-07-29 05:42:52 +0000

    Ship traveling time page added

---
+++ ship-traveling-time.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ship Traveling Time Command Modern Air/Naval Operations (CMANO) PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Ship Traveling Time Command Modern Air/Naval Operations (CMANO) PMC Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Ship Traveling Time Command Modern Air/Naval Operations (CMANO) PMC Tactical</h1>
+</header>
+
+<section>
+	<h2>Ship Traveling Time</h2>
+
+<p>
+CVN 68 Nimitz traveling 15 knots from The United States New York coast to Iceland Keflavik takes from July 5th 1200hrs to July 12th 0138hrs. This is over <b>ONE WEEK</b> of traveling at 15 knots.
+</p>
+
+<p>
+Just wanted to learn how long it takes to travel that distance (at that speed?), I don't think it would make a very enjoyable mission/scenario if you started from NY ie US east coast, with atlantic ocean full of soviet subs and then you were greeted in the end with fleet of badger/backfire bombers from Iceland. Such scenario needs to be split into several smaller segments to keep it interesting or "manageable".
+</p>
+
+<p>
+I love long slow brewing missions/scenarios with hours of boredom and seconds of terror, however this kind of time accelerating for in-game days is quite extreme, although if the atlantic is peppered with random soviet commie scum subs then your P3C Orion and other fleet borne anti submarine warware (ASW) aircrafts/helicopters have their hands full so time accelerating a high speed might be problematic.
+</p>
+
+<p>
+Dunno, maybe New York to Iceland transit would be best to split into maybe 4 smaller more focused missions/scenarios. Depends on the campaign style of course.
+</p>
+
+</section>
+
+<footer>
+<p>Back to <a href="index.php">PMC CMANO root page</a></p>
+<br><br>
+<p><i>PMC CMANO 2015 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

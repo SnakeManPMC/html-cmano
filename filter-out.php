2ccea7e6f7c7bbb4e32b2b949034b54b6a4546e0
Snake Man <snakeman@phnet.fi>
2024-07-29 04:51:19 +0000

    Index page edit, filter-out page added

---
+++ filter-out.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Filter Out Command Modern Air/Naval Operations (CMANO) PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Filter Out Command Modern Air/Naval Operations (CMANO) PMC Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Filter Out Command Modern Air/Naval Operations (CMANO) PMC Tactical</h1>
+</header>
+
+<section>
+	<h2>Filter Out Contacts</h2>
+
+<p>
+2023-07-11T11:28:00Z
+</p>
+
+<p>
+When you get an contact, either air, surface or sub-surface, its first unknown yellow, once its identified it can turn into friendly, neutral or hostile, at this point you can right click the unit icon and select "Filter-out" from the dialog.
+</p>
+
+<p>
+At this time I am not sure if these units stay filtered out forever, or if you lose the contact and regain it later will the filter-out be remembered or do you have to do it all over again.
+</p>
+
+<p>
+Dimitris posted in steamshit community forum: <i>Filtering out a contact removes all visual info about it (uncertainty area, emissions, datablock etc.) and leaves only the contact icon, significantly de-cluttering the display. Please read the manual addendum pages: <a href="http://www.warfaresims.com/?page_id=2695" target="_blank">warfaresims.com/?page_id=2695</a>.</i>
+</p>
+
+</section>
+
+<footer>
+<p>Back to <a href="index.php">PMC CMANO root page</a></p>
+<br><br>
+<p><i>PMC CMANO 2015 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <title>Command Modern Air/Naval Operations (CMANO) - PMC Tactical</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="Command Modern Air/Naval Operations (CMANO) - PMC Tactical">
-<META name="keywords" content="Command, Modern, Air, Naval, Operations, CMANO, PMC, Tactical">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,50 +13,32 @@
 <body>
 
 <header>
-<?php include("../include/google-ads.php"); ?>
-<h1>PMC Tactical - Command Modern Air/Naval Operations (CMANO)</h1>
+<h1>Command Modern Air/Naval Operations PMC Tactical</h1>
 </header>
 
 <section>
-<h2>Command Modern Air/Naval Operations (CMANO)</h2>
-<p>It all started to game called Harpoon back in the 1989. CMANO: Northern Inferno is said to be very close to what we old late 80's Harpoon players remember.
+	<h2>Command Modern Air/Naval Operations (CMANO)</h2>
+
+<p>
+It all started to game called Harpoon back in the 1989. CMANO: Northern Inferno is said to be very close to what we old late 80's Harpoon players remember.
 </p>
 
-<p>CMANO is developed by Warfare Sims, published by Matrix Games and released on September 24, 2013.
+<p>
+CMANO is developed by Warfare Sims, published by Matrix Games and released on September 24, 2013. Later it was renamed to Command Modern Operations (CMO).
 </p>
 
-<p><a href="file-format.php">File Format</a> of scenario and campaign files.
-</p>
-
-<p><a href="tutorial-import-export-files.php">Tutorial - Import Export Files</a>, how to save your units and groups to a file.
-</p>
-
-<p><a href="tutorial-event-editor.php">Tutorial Event Editor</a>
-</p>
-
-<p><a href="scenario-design.php">Scenario Design</a> ideas, tips, etc
-</p>
-
-<p><a href="scenario-editing-basics.php">Scenario Editing Basics</a>
-</p>
-
-<p><a href="scenario-play-throughs.php">Scenario Play Throughs</a>
-</p>
-
-<p><a href="unable-to-load-dll-lua52.php">How to fix error: Unable to load DLL lua52</a>
-</p>
-
-<p><a href="history.php">History</a>
-</p>
-
-<p><a href="settings.php">Settings</a>, how to optimize performance etc.
-</p>
-
-<p><a href="links.php">Links</a>
-</p>
-
-<p><a href="downloads.php">Downloads</a>
-</p>
+<p><a href="downloads.php">Downloads</a></p>
+<p><a href="file-format.php">File Format</a> of scenario and campaign files.</p>
+<p><a href="filter-out.php">Filter Out</a> contracts to de-clutter world map.</p>
+<p><a href="history.php">History</a></p>
+<p><a href="links.php">Links</a></p>
+<p><a href="scenario-design.php">Scenario Design</a> ideas, tips, etc</p>
+<p><a href="scenario-editing-basics.php">Scenario Editing Basics</a></p>
+<p><a href="scenario-play-throughs.php">Scenario Play Throughs</a></p>
+<p><a href="settings.php">Settings</a>, how to optimize performance etc.</p>
+<p><a href="tutorial-event-editor.php">Tutorial Event Editor</a></p>
+<p><a href="tutorial-import-export-files.php">Tutorial - Import Export Files</a>, how to save your units and groups to a file.</p>
+<p><a href="unable-to-load-dll-lua52.php">How to fix error: Unable to load DLL lua52</a></p>
 
 </section>
 

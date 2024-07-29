087ea3c104aaa3d5b0004567c0a836db6dd39adf
Snake Man <snakeman@phnet.fi>
2024-07-29 06:22:49 +0000

    Scenario editor choose database page added

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ CMANO is developed by Warfare Sims, published by Matrix Games and released on Se
 <p><a href="scenario-design.php">Scenario Design</a> ideas, tips, etc</p>
 <p><a href="scenario-editing-basics.php">Scenario Editing Basics</a></p>
 <p><a href="scenario-editor-ai-naval-strike.php">Scenario Editor AI Naval Strike</a></p>
+<p><a href="scenario-editor-choose-database.php">Scenario Editor Choose Database</a></p>
 <p><a href="scenario-editor-soviet-submarines.php">Scenario Editor Soviet Submarines</a></p>
 <p><a href="scenario-play-throughs.php">Scenario Play Throughs</a></p>
 <p><a href="settings.php">Settings</a>, how to optimize performance etc.</p>
---
+++ scenario-editor-choose-database.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Scenario Editor Choose Database Command Modern Air/Naval Operations (CMANO) PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Scenario Editor Choose Database Command Modern Air/Naval Operations (CMANO) PMC Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Scenario Editor Choose Database Command Modern Air/Naval Operations (CMANO) PMC Tactical</h1>
+</header>
+
+<section>
+	<h2>Scenario Editor Choose Database</h2>
+
+<p>
+Scenario Editor -&gt; Database -&gt; which ever database you prefer.
+</p>
+
+<p>
+This must be done right after creating a new scenario, before adding sides!
+</p>
+
+</section>
+
+<footer>
+<p>Back to <a href="index.php">PMC CMANO root page</a></p>
+<br><br>
+<p><i>PMC CMANO 2015 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
